a68f4eecc1f960b93ad2be781cc020a078fe8a24
DataNick <DataNick@users.noreply.github.com>
2018-07-03 21:30:21 -0400

    Write failing test

--- spec/bowling_game_spec.rb
+++ spec/bowling_game_spec.rb
@@ -7,4 +7,12 @@ RSpec.describe BowlingGame do
   it "knows the score is 0 for a new game" do
     expect(game.score).to eq(0)
   end
+
+  it "should return a score of 0 for 0 pins knocked down" do
+    10.times do |roll|
+      game.roll(0)
+    end
+
+    expect(game.score).to eq(0)
+  end
 end
