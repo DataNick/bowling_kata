40d98a517f6d667500059f36fd5682508f16ea0d
DataNick <DataNick@users.noreply.github.com>
2018-07-05 01:46:11 -0400

    Add third test

--- spec/bowling_game_spec.rb
+++ spec/bowling_game_spec.rb
@@ -15,4 +15,12 @@ RSpec.describe BowlingGame do
 
     expect(game.score).to eq(0)
   end
+
+  it "should return a score of 20 after each roll of 1 pin" do
+    20.times do  |roll|
+      game.roll(1)
+    end
+
+    expect(game.score).to eq(20)
+  end
 end
